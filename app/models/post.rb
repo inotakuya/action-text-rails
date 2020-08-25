536886121e222e5cb9b0d6c1844d4ab8d54201dc
inoue takuya <i_no_u_u@yahoo.co.jp>
2020-08-26 07:34:35 +0900

    画像サイズもバリデーションするように修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,19 +2,39 @@ class Post < ApplicationRecord
   has_rich_text :content
 
   validates :title, length: { maximum: 32 }, presence: true
+  validate :validate_content_length
+  validate :validate_content_attachment_byte_size
 
   MAX_CONTENT_LENGTH = 50
-  validate :validate_content_length
+  ONE_KILOBYTE = 1024
+  MEGA_BYTES = 4
+  MAX_CONTENT_ATTACHMENT_BYTE_SIZE = ONE_KILOBYTE * 1_000 * MEGA_BYTES
 
-  def validate_content_length
-    length = content.to_plain_text.length
-    if length > MAX_CONTENT_LENGTH
-      errors.add(
-          :content,
-          :too_long,
-          max_content_length: MAX_CONTENT_LENGTH,
-          length: length
-      )
+  private
+
+    def validate_content_length
+      length = content.to_plain_text.length
+      if length > MAX_CONTENT_LENGTH
+        errors.add(
+            :content,
+            :too_long,
+            max_content_length: MAX_CONTENT_LENGTH,
+            length: length
+        )
+      end
     end
-  end
+
+    def validate_content_attachment_byte_size
+      content.body.attachables.grep(ActiveStorage::Blob).each do |attachable|
+        if attachable.byte_size > MAX_CONTENT_ATTACHMENT_BYTE_SIZE
+          errors.add(
+              :base,
+              :content_attachment_byte_size_is_too_big,
+              max_content_attachment_mega_byte_size: MEGA_BYTES,
+              bytes: attachable.byte_size,
+              max_bytes: MAX_CONTENT_ATTACHMENT_BYTE_SIZE
+          )
+        end
+      end
+    end
 end
